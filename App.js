0926617003466cfb9de6da8b5a2e424090ee7902
danocolombo@google.com <GR0mans1212!>
2021-05-19 14:46:27 -0400

    fixed if statement

--- App.js
+++ App.js
@@ -12,7 +12,9 @@ import { theme } from "./src/infrastructure/theme";
 export default function App() {
   const [oswaldLoaded] = useOswald({ Oswald_400Regular });
   const [latoLoaded] = useLato({ Lato_400Regular });
-  if (!oswaldLoaded || !latoLoaded) return null;
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
   return (
     <>
       <ThemeProvider theme={theme}>
