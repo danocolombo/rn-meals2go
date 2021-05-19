400dcfbd90bf4ae0e551179298aa3135cdb889e5
danocolombo <danocolombo@gmail.com>
2021-05-18 21:21:16 -0400

    restaurant-into-card converted to styled-components

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,6 +1,21 @@
 import React from "react";
+import styled from "styled-components/native";
 import { Card } from "react-native-paper";
 import { Text, StyleSheet } from "react-native";
+
+const Title = styled.Text`
+  padding: 10px;
+  font-size: 20px;
+  color: blue;
+  font-weight: bold;
+`;
+const RestaurantCard = styled(Card)`
+  background-color: white;
+`;
+const RestaurantCardCover = styled(Card.Cover)`
+  padding: 20px;
+  background-color: white;
+`;
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = "Louie's",
@@ -15,23 +30,10 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
   } = restaurant;
   return (
     <>
-      <Card elevation={5} style={styles.card}>
-        <Card.Cover
-          key={name}
-          style={styles.cover}
-          source={{ uri: photos[0] }}
-        />
-        <Text style={styles.title}>{name}</Text>
-      </Card>
+      <RestaurantCard elevation={5}>
+        <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
+        <Title>{name}</Title>
+      </RestaurantCard>
     </>
   );
 };
-const styles = StyleSheet.create({
-  card: {
-    backgroundColor: "white",
-  },
-  title: {
-    padding: 20,
-  },
-  cover: { padding: 20, backgroundColor: "white" },
-});
