dfebab640d2a7d781351977023543ba8a5c017eb
danocolombo@google.com <GR0mans1212!>
2021-05-19 14:55:01 -0400

    card displaying and stars(rating) displayed

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,22 +1,43 @@
 import React from "react";
 import styled from "styled-components/native";
+import { Text, StyleSheet } from "react-native";
 import { Card } from "react-native-paper";
+import { SvgXml } from "react-native-svg";
 
-const Title = styled.Text`
-  padding: ${(props) => props.theme.space[3]};
-  font-family: ${(props) => props.theme.fonts.body};
-`;
-//font-size: ${(props) => props.theme.fonts.fontSizes.body};
+import star from "../../../../assets/star";
+
 const RestaurantCard = styled(Card)`
   background-color: ${(props) => props.theme.colors.bg.primary};
 `;
+
 const RestaurantCardCover = styled(Card.Cover)`
-  padding: ${(props) => props.theme.space[2]};
+  padding: ${(props) => props.theme.space[3]};
   background-color: ${(props) => props.theme.colors.bg.primary};
+`;
+
+const Address = styled(Text)`
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption};
+`;
+
+const Title = styled(Text)`
+  font-family: ${(props) => props.theme.fonts.heading};
+  font-size: ${(props) => props.theme.fontSizes.body};
+  color: ${(props) => props.theme.colors.ui.primary};
+`;
+
+const Info = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const Rating = styled.View`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]};
 `;
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
-    name = "Louie's",
+    name = "Louie's Pasta",
     icon,
     photos = [
       "https://media.istockphoto.com/photos/pasta-with-meat-tomato-sauce-and-vegetables-picture-id857927726?b=1&k=6&m=857927726&s=170667a&w=0&h=2PsKzIHncOet6jE3JAA40Oh8d97BUvnZk421Yeb2gww=",
@@ -26,12 +47,20 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
     rating = 4,
     isClosedTemporarily,
   } = restaurant;
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
+
   return (
-    <>
-      <RestaurantCard elevation={5}>
-        <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
+    <RestaurantCard elevation={5}>
+      <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
+      <Info>
         <Title>{name}</Title>
-      </RestaurantCard>
-    </>
+        <Rating>
+          {ratingArray.map(() => (
+            <SvgXml xml={star} width={20} height={20} />
+          ))}
+        </Rating>
+        <Address>{address}</Address>
+      </Info>
+    </RestaurantCard>
   );
 };
